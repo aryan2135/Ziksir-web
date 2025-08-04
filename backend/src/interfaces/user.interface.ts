00b4be57fcc874153381960e070d14878850447c
AdityaRaj212 <aditya.education212@gmail.com>
2025-08-04 20:15:25 +0530

    modified user schema so that organization level info can also be stored

--- backend/src/interfaces/user.interface.ts
+++ backend/src/interfaces/user.interface.ts
@@ -3,11 +3,33 @@ export interface User extends Document {
     email: string;
     password: string;
     role: 'admin' | 'user';
+    contactNo: string;
+    emailId: string;
+    organizationAddress: string;
+    state: string;
+    country: string;
+    gstin: string;
+    gstinNo: string;
+    panNo: string;
+    pincode: number;
+    remarks: string;
+    organizationCategory: string;
 };
 
 export interface UserData { 
     name: string; 
     email: string; 
     password: string; 
-    role: 'admin' | 'user' 
+    role: 'admin' | 'user';
+    contactNo: string;
+    emailId: string;
+    organizationAddress: string;
+    state: string;
+    country: string;
+    gstin: string;
+    gstinNo: string;
+    panNo: string;
+    pincode: number;
+    remarks: string;
+    organizationCategory: string;
 };
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -5,7 +5,18 @@ const userSchema = new Schema<UserInterface>({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  role: { type: String, enum: ['admin', 'user'], default: 'user' }
+  role: { type: String, enum: ['admin', 'user'], default: 'user' },
+  contactNo: {type: String, required: false},
+  emailId: {type: String, required: false},
+  organizationAddress: {type: String, required: false},
+  state: {type: String, required: false},
+  country: {type: String, required: false},
+  gstin: {type: String, required: false},
+  gstinNo: {type: String, required: false},
+  panNo: {type: String, required: false},
+  pincode: {type: Number, required: false},
+  remarks: {type: String, required: false},
+  organizationCategory: {type: String, required: false}
 });
 
 export const User = mongoose.model<UserInterface>('User', userSchema);
