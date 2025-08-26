a9556d6a73a6cf6cc7570377be6246956f8f9d89
Ankitjordan <ankityar13@gmail.com>
2025-08-27 03:09:43 +0530

    fixing cookies authorization

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -2,6 +2,9 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { env } from "../config/env";
 import { NextFunction, Request, Response } from "express";
 
+const isProd = env.NODE_ENV === "production";
+const name = isProd ? "__Host-session" : "authToken";
+
 export const generateToken = (id: string) => {
   return jwt.sign({ id }, env.JWT_SECRET!, { expiresIn: "7d" });
 };
@@ -11,7 +14,7 @@ export const verifyToken = (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.cookies.authToken;
+  const token = req.cookies[name];
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token" });
